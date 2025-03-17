7e53928339b4ea3b2ccea7e86120d8595ace06b4
for-you-Nikita <troickiy09@gmail.com>
2025-03-17 11:50:24 +0300

    описание моделей SQLAlchemy категорий и продуктов

--- app/models/category.py
+++ app/models/category.py
@@ -1,5 +1,5 @@
 from app.backend.db import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.models.products import Product
 
@@ -11,6 +11,7 @@ class Category(Base):
     name = Column(String)
     slug = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
+    parent_id = Column(Integer, ForeignKey("categories.id"), nullable=True)
 
     products = relationship("Product", back_populates="category")
 
