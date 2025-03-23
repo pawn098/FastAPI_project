70245d586c7ebdaa61e19602303a25b09e962277
for-you-Nikita <troickiy09@gmail.com>
2025-03-23 21:54:23 +0300

    commit permission

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,31 +1,103 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, insert
 from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
+import jwt
 
 from app.models.user import User
-from app.backend.db_depends import get_db
 from app.schemas import CreateUser
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from app.backend.db_depends import get_db
 
 
+SECRET_KEY = "a10dd846ef16125290e0f31120eeaa403d14b68622975c4acc100903ae62cab4"
+ALGORITHM = 'HS256'
 
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
-security = HTTPBasic()
-async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)],
-                               credentials: HTTPBasicCredentials = Depends(security)):
-    user = await db.scalar(select(User).where(User.username == credentials.username))
-    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
+    user = await db.scalar(select(User).where(User.username == username))
+    if not user or not bcrypt_context.verify(password, user.hashed_password) or user.is_active == False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
-@router.get('/users/me')
-async def read_current_user(user: dict = Depends(get_current_username)):
-    return {'User': user}
+async def create_access_token(username: str, user_id: int, is_admin: bool, is_supplier: bool, is_customer: bool,
+                              expires_delta: timedelta):
+    payload = {
+        'sub': username,
+        'id': user_id,
+        'is_admin': is_admin,
+        'is_supplier': is_supplier,
+        'is_customer': is_customer,
+        'exp': datetime.now(timezone.utc) + expires_delta
+    }
+    payload['exp'] = int(payload['exp'].timestamp())
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[AsyncSession, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str | None = payload.get('sub')
+        user_id: int | None = payload.get('id')
+        is_admin: bool | None = payload.get('is_admin')
+        is_supplier: bool | None = payload.get('is_supplier')
+        is_customer: bool | None = payload.get('is_customer')
+        expire: int | None = payload.get('exp')
+
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Не удалось проверить пользователя'
+            )
+
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Токен доступа не предоставлен'
+            )
+
+        if not isinstance(expire, int):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Неверный формат токена'
+            )
+
+        current_time = datetime.now(timezone.utc).timestamp()
+
+        if expire < current_time:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Срок действия токена истек!'
+            )
+
+        return {
+            'username': username,
+            'id': user_id,
+            'is_admin': is_admin,
+            'is_supplier': is_supplier,
+            'is_customer': is_customer
+        }
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Срок действия токена истек!'
+        )
+    except jwt.exceptions:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Не удалось проверить пользователя'
+        )
+
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user: CreateUser):
@@ -33,10 +105,27 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user:
                                          last_name=create_user.last_name,
                                          username=create_user.username,
                                          email=create_user.email,
-                                         hashed_password=bcrypt_context.hash(create_user.password)))
+                                         hashed_password=bcrypt_context.hash(create_user.password),
+                                         ))
     await db.commit()
     return {
         'status_code': status.HTTP_201_CREATED,
         'transaction': 'Successful'
     }
 
+
+@router.post('/token')
+async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+
+    token = await create_access_token(user.username, user.id, user.is_admin, user.is_supplier, user.is_customer,
+                                expires_delta=timedelta(minutes=20))
+
+    return {
+        'access_token': token,
+        'token_type': 'bearer'
+    }
+
+@router.get('/read_current_user')
+async def read_current_user(user: dict = Depends(get_current_user)):
+    return {'user': user}
